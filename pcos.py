79e80e520a88e4c1191f59512dfc039f800bbb5b
Neelam Swain <neelamswain26@gmail.com>
2024-07-04 13:14:59 +0530

    pcos.py

--- pcos.py
+++ pcos.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import scikit-learn as sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
